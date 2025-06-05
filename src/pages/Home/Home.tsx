e29acab08bac2eb20c1e00c40fd5641c938f2527
Nurdaulet Bagdauletov <bagdauletovnurdaulet11@gmail.com>
2025-06-05 11:43:52 +0500

    Add PhotoBox component styles and global type declarations
    - Created PhotoBox.module.css for styling the PhotoBox component with responsive sizes (small, medium, large) and circular shape.
    - Added source map for PhotoBox.module.css.
    - Introduced global type declarations for various file types including CSS, SCSS, SVG, PNG, JPG, JPEG, and WEBP to improve TypeScript support.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,31 @@
-import avatar from '@/assets/images/avatar.png';
+import avatar from '@/assets/images/avatar_large.png';
 import PhotoBox from '@/components/PhotoBox/PhotoBox';
 
 import styles from './Home.module.scss';
 
 const Home = () => {
 	return (
-		<div>
-			<PhotoBox src={avatar} alt='User avatar' size='large' />
-			<PhotoBox src={avatar} alt='User avatar' size='medium' />
-			<PhotoBox src={avatar} alt='User avatar' size='small' />
-		</div>
+		<main className={styles.home}>
+			<section className={styles['home__intro']}>
+				<PhotoBox
+					className={styles['home_avatar']}
+					src={avatar}
+					size='large'
+					alt='User avatar'
+				/>
+				<h1 className={styles['home__name']}>John Doe</h1>
+
+				<p className={styles['home__role']}>Programmer. Creative. Innovator</p>
+				<p className={styles['home__desc']}>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+					commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+				</p>
+
+				<a href='/resume' className={styles['home__button']}>
+					View Resume
+				</a>
+			</section>
+		</main>
 	);
 };
 
