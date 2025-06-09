51e6f22c07e96d2626c1f64a69061d69b4679a94
Nurdaulet Bagdauletov <bagdauletovnurdaulet11@gmail.com>
2025-06-09 16:52:07 +0500

    feat: update EducationSection with additional education entries and enhance Timeline styling

--- src/pages/Resume/components/EducationSection/EducationSection.tsx
+++ src/pages/Resume/components/EducationSection/EducationSection.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-
 import SectionLayout from '@/components/SectionLayout/SectionLayout';
 import Timeline from '@/components/Timline/Timeline';
-
-import styles from './EducationSection.module.scss';
 
 const educationData = [
 	{
 		date: '2020',
 		title: 'High School Diploma',
 		text: 'Graduated from School-Lyceum №15, Kyzylorda.',
+	},
+	{
+		date: '2021',
+		title: 'Lorem ipsum dolor sit.',
+		text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nobis eius, consequuntur, perspiciatis odit magnam voluptatibus aliquam earum vitae adipisci quisquam laudantium architecto ipsum iure id obcaecati quas ea nihil, quae quaerat. Qui est nobis voluptatum error quod at, voluptates assumenda neque sint ut ab porro iste dolores id facere sequi doloribus illum quam. Doloremque, veniam. Perferendis sapiente aut officiis dolor distinctio id nesciunt atque veritatis culpa, assumenda repellat commodi?',
+	},
+	{
+		date: '2022',
+		title: 'Lorem ipsum dolor sit amet consectetur.',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, repellendus excepturi, deleniti accusantium iste mollitia quae alias ipsa numquam eius doloribus iure tempora quo eveniet possimus labore voluptates a quod.',
+	},
+	{
+		date: '2023',
+		title: 'Lorem, ipsum dolor.',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab rem repellendus, odit excepturi obcaecati iure sapiente pariatur laboriosam error beatae. Harum unde error debitis officia illum quo est molestiae nam cupiditate dignissimos sequi esse, nisi incidunt perferendis distinctio dolorum corrupti?',
 	},
 	{
 		date: '2024',
